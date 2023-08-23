c550c30c27b2758abd36c5cd5b1afccfba0815ad
CAotsuki <otsuki.atsushi@cloud-ace.jp>
2023-08-23 16:29:24 +0900

    add hello world

--- cmd/main.go
+++ cmd/main.go
@@ -5,30 +5,35 @@ import (
 	"log"
 	"net/http"
 	"os"
-
-	"sandbox/pkg/controller"
-	"sandbox/pkg/model/repository"
 )
 
-var tr = repository.NewTodoRepository()
-var tc = controller.NewTodoController(tr)
-var ro = controller.NewRouter(tc)
-
 func main() {
-	port := os.Getenv("API_PORT")
+	log.Print("starting server...")
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/error", errHandler)
+
+	// Determine port for HTTP service.
+	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
+		log.Printf("defaulting to port %s", port)
 	}
-	log.Printf("Listening on port %s", port)
 
-	http.HandleFunc("/", homeLink)
-	http.HandleFunc("/todos/", ro.HandleTodoRequest)
-
+	// Start HTTP server.
+	log.Printf("listening on port %s", port)
 	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func homeLink(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Welcome home!")
+func handler(w http.ResponseWriter, r *http.Request) {
+	name := os.Getenv("NAME")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!\n", name)
+}
+
+func errHandler(w http.ResponseWriter, r *http.Request) {
+	http.Error(w, "Hello Error!\n", http.StatusInternalServerError)
 }
